3236c740d6a9f79d9181deb06262ab3f278534ff
stian <stianpiano@gmail.com>
2023-12-10 18:33:05 +0100

    Fixed typo

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ const Display = (props) => {
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const incremetCounter = (incrementValue) => setCounter(counter + incrementValue);
+  const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
 
   return (
     <div>
-      <Button onClickFunction={incremetCounter} increment={1}/>
-      <Button onClickFunction={incremetCounter}  increment={5}/>
-      <Button onClickFunction={incremetCounter}  increment={10}/>
-      <Button onClickFunction={incremetCounter}  increment={100}/>
+      <Button onClickFunction={incrementCounter} increment={1}/>
+      <Button onClickFunction={incrementCounter}  increment={5}/>
+      <Button onClickFunction={incrementCounter}  increment={10}/>
+      <Button onClickFunction={incrementCounter}  increment={100}/>
       <Display message={counter} />
     </div>
   );
