894440e51e889d62e23ac80aefbfe2a4abeccf8d
stian <stianpiano@gmail.com>
2023-12-10 18:14:09 +0100

    Added message

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 
 const Button = (props) => {
   const handleOnClick = () => props.onClickFunction(props.increment);
-  
+
   return (
   <button onClick={handleOnClick}>
     +{props.increment}
   </button>
+  );
+};
+
+const Display = (props) => {
+  return (
+    <div>{props.message}</div>
   );
 };
 
@@ -21,6 +27,7 @@ function App() {
       <Button onClickFunction={incremetCounter}  increment={5}/>
       <Button onClickFunction={incremetCounter}  increment={10}/>
       <Button onClickFunction={incremetCounter}  increment={100}/>
+      <Display message={counter} />
     </div>
   );
 }
