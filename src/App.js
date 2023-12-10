bd73b9119e6bc1d918ee87ad082c75d57b708547
stian <stianpiano@gmail.com>
2023-12-10 18:12:21 +0100

    Button onClick

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
+import React, { useState} from "react";
 import './App.css';
 
 const Button = (props) => {
-  return <button>1</button>
+  const handleOnClick = () => props.onClickFunction(props.increment);
+  
+  return (
+  <button onClick={handleOnClick}>
+    +{props.increment}
+  </button>
+  );
 };
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const incremetCounter = (incrementValue) = setCounter(counter + incrementValue);
+
   return (
     <div>
-      <Button />
+      <Button onClickFunction={incremetCounter} increment={1}/>
+      <Button onClickFunction={incremetCounter}  increment={5}/>
+      <Button onClickFunction={incremetCounter}  increment={10}/>
+      <Button onClickFunction={incremetCounter}  increment={100}/>
     </div>
   );
 }
